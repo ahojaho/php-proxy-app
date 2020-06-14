db1afb5691d13fdb0faec03ec4d0856b02ad1923
ahojaho <65909896+ahojaho@users.noreply.github.com>
2020-06-14 13:17:56 +0200

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $config = array();
 $config['app_key'] = 'irskotest';
 
 // a secret key to be used during encryption
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'superheslo';
 
 // URL running the proxy app
 //$config['app_url'] = 'https://www.mysampleproxy.com/proxyfolder/';
